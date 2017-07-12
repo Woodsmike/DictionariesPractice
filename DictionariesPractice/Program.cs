15d857c0a8b258aebe864024dbbd5ca53f564df9
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-12 15:29:18 -0400

    2 of 3 problems done

--- DictionariesPractice/Program.cs
+++ DictionariesPractice/Program.cs
@@ -24,7 +24,8 @@ namespace DictionariesPractice
             {
                 Console.WriteLine(item);
             } */
-            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+
+            /*Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
 
             drinkMachine.Add(10, "Ginger Ale");
             drinkMachine.Add(11, "Bottled Water");
@@ -32,11 +33,77 @@ namespace DictionariesPractice
             drinkMachine.Add(13, "Orange Juice");
             drinkMachine.Add(14, "Fruit Punch");
 
-            Console.WriteLine(drinkMachine.Count);
+            //Console.WriteLine(drinkMachine.Count);
             foreach (KeyValuePair<int, string> drink in drinkMachine)
             {
+                Console.WriteLine(drink.Key);
                 Console.WriteLine(drink.Value);
+            }*/
+
+            //create a dictionary for a theatre coat check with 10 elements
+            //key number, value will be the coat style
+
+            Dictionary<int, string> coatCheck = new Dictionary<int, string>();
+            coatCheck.Add(1, "Black Leather Jacket");
+            coatCheck.Add(2, "Long Rain Coat");
+            coatCheck.Add(3, "Fur Coat");
+            coatCheck.Add(4, "Bomber Jacket");
+            coatCheck.Add(5, "Black Leather Jacket");
+            coatCheck.Add(6, "Business Suit Jacket");
+            coatCheck.Add(7, "Blazer");
+            coatCheck.Add(8, "Long Rain Coat");
+            coatCheck.Add(9, "Black Leather Jacket");
+            coatCheck.Add(10, "Long Rain Coat");
+
+            foreach(KeyValuePair<int, string> coat in coatCheck)
+            {
+                Console.WriteLine(coat);
             }
+
+            //Car valet with 10 cars, the key will be a customer last name and the 
+            //value will be the car make.  Print all elements to the console.
+
+            Dictionary<string, string> carValet = new Dictionary<string, string>();
+
+            carValet.Add("Smith", "Mustang");
+            carValet.Add("Smith2", "Corvette");
+            carValet.Add("King", "Suzuki");
+            carValet.Add("Woods", "Ferrari");
+            carValet.Add("Lewis", "Smart Car");
+            carValet.Add("Johnson", "Limo");
+            carValet.Add("Vivacqua", "Camaro SS");
+            carValet.Add("Batman", "Batmobile");
+            carValet.Add("Turner", "Mercedes 500");
+            carValet.Add("Holloway", "BMW M5");
+
+            foreach (KeyValuePair<string, string> car in carValet)
+            {
+                Console.WriteLine(car);
+            }
+
+            //Create a dictionary of 10 zoo animals.  The key will be the animal type.
+            //The value will be the number of animals.
+            //Print the animal with the highest quantity to the console.
+            //Remove the zoo animal with the lowest quantity using the remove()
+
+            Dictionary<string, int> zooAnimal = new Dictionary<string, int>();
+
+            zooAnimal.Add("Zebras", 5);
+            zooAnimal.Add("Giraffes", 4);
+            zooAnimal.Add("Lions", 3);
+            zooAnimal.Add("Koala Bears", 2);
+            zooAnimal.Add("Apes", 2);
+            zooAnimal.Add("Monkies", 6);
+            zooAnimal.Add("King Cobras", 2);
+            zooAnimal.Add("Gazelles", 4);
+            zooAnimal.Add("Birds", 20);
+            zooAnimal.Add("Polar Bears", 1);
+
+        
+            {
+                ;                
+            }
+
         }
     }
 }
