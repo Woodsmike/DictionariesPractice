708e78bc717b6c04eab0222232492eb71478b998
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-12 14:04:33 -0400

    First practice problem

--- DictionariesPractice/Program.cs
+++ DictionariesPractice/Program.cs
@@ -10,6 +10,33 @@ namespace DictionariesPractice
     {
         static void Main(string[] args)
         {
+            //Dictionaries - each key has to be unique....the value can be the same
+            //for different keys.  Key Value Pair.
+            //syntax:  Dictionary<string, string> snackMachine = new Dictionay<string, string>() 
+            /*Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            {
+                {"A1", "Twix" },
+                {"A2", "Twizzlers" },
+                {"A3", "Reeses" },
+                {"A4", "Sour Patch Kids" }
+            };
+            foreach (KeyValuePair<string, string> item in snackMachine)
+            {
+                Console.WriteLine(item);
+            } */
+            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+
+            drinkMachine.Add(10, "Ginger Ale");
+            drinkMachine.Add(11, "Bottled Water");
+            drinkMachine.Add(12, "Mountain Dew");
+            drinkMachine.Add(13, "Orange Juice");
+            drinkMachine.Add(14, "Fruit Punch");
+
+            Console.WriteLine(drinkMachine.Count);
+            foreach (KeyValuePair<int, string> drink in drinkMachine)
+            {
+                Console.WriteLine(drink.Value);
+            }
         }
     }
 }
